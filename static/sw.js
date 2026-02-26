3c98eba3d15d01d8e89ff51a429bae9b4b174ccf
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-02-26 10:36:01 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,28 +1,30 @@
-const CACHE_NAME = "aura-mobile-v1";
+const CACHE_NAME = "aura-mobile-v3"; // change version when update
 
-const ASSETS = [
+const CORE_ASSETS = [
   "/",
   "/index.html",
   "/manifest.json"
 ];
 
-// install
+// INSTALL
 self.addEventListener("install", (e) => {
   self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
+      return cache.addAll(CORE_ASSETS);
     })
   );
 });
 
-// activate
+// ACTIVATE
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
         keys.map(k => {
-          if (k !== CACHE_NAME) return caches.delete(k);
+          if (k !== CACHE_NAME) {
+            return caches.delete(k);
+          }
         })
       )
     )
@@ -30,7 +32,7 @@ self.addEventListener("activate", (e) => {
   self.clients.claim();
 });
 
-// fetch
+// FETCH
 self.addEventListener("fetch", (e) => {
   if (!e.request.url.startsWith("http")) return;
 
@@ -38,15 +40,17 @@ self.addEventListener("fetch", (e) => {
     caches.match(e.request).then(res => {
       if (res) return res;
 
-      return fetch(e.request).then(network => {
-        const clone = network.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(e.request, clone);
+      return fetch(e.request)
+        .then(network => {
+          const clone = network.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(e.request, clone);
+          });
+          return network;
+        })
+        .catch(() => {
+          return caches.match("/index.html");
         });
-        return network;
-      }).catch(() => {
-        return caches.match("/index.html");
-      });
     })
   );
 });
