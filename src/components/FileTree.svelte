f72ad025b3d70a8e33509257132022177ed0cf9d
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-02-26 10:40:21 +0530

    Update FileTree.svelte

--- src/components/FileTree.svelte
+++ src/components/FileTree.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import AuraFS from "../core/auraFS.js";
-  import { activeFileId, setActiveFile, setSelectedFolder } from "../stores/projectStore.js";
+  import TreeItem from "./TreeItem.svelte";
 
   let tree = [];
 
@@ -14,55 +14,6 @@
     window.addEventListener("refresh-tree", refresh);
     return () => window.removeEventListener("refresh-tree", refresh);
   });
-
-  function toggleFolder(node) {
-    node.isOpen = !node.isOpen;
-    AuraFS.saveToDB();
-    refresh();
-    setSelectedFolder(node.id);
-  }
-
-  function openFile(node) {
-    setActiveFile(node.id);
-    window.dispatchEvent(new CustomEvent("open-file", { detail: node.id }));
-  }
-
-  function newFile(parentId) {
-    const name = prompt("File name:", "file.js");
-    if (!name) return;
-    AuraFS.addNode(parentId, name, "file", "");
-    refresh();
-  }
-
-  function newFolder(parentId) {
-    const name = prompt("Folder name:", "folder");
-    if (!name) return;
-    AuraFS.addNode(parentId, name, "folder");
-    refresh();
-  }
-
-  function rename(node) {
-    const name = prompt("Rename:", node.name);
-    if (!name) return;
-    node.name = name;
-    AuraFS.saveToDB();
-    refresh();
-  }
-
-  function del(node) {
-    if (!confirm("Delete?")) return;
-    AuraFS.deleteNode(node.id);
-    refresh();
-  }
-
-  function getIcon(node) {
-    if (node.type === "folder") return node.isOpen ? "ri-folder-open-fill" : "ri-folder-fill";
-    if (node.name.endsWith(".js")) return "ri-javascript-fill";
-    if (node.name.endsWith(".jsx")) return "ri-reactjs-line";
-    if (node.name.endsWith(".css")) return "ri-css3-fill";
-    if (node.name.endsWith(".html")) return "ri-html5-fill";
-    return "ri-file-code-fill";
-  }
 </script>
 
 <div class="text-sm select-none">
@@ -75,40 +26,4 @@
   {#each tree as node}
     <TreeItem {node} depth={0}/>
   {/each}
-</div>
-
-<script>
-  export let node;
-  export let depth = 0;
-</script>
-
-<div class="group">
-  <div
-    class="flex items-center pr-2 hover:bg-soft transition"
-    class:bg-accent={$activeFileId === node.id}
-    style="padding-left:{depth * 16 + 14}px;height:42px"
-  >
-    <div
-      class="flex-1 flex items-center gap-2 overflow-hidden"
-      on:click={() => node.type === 'folder' ? toggleFolder(node) : openFile(node)}
-    >
-      <i class="{getIcon(node)} text-zinc-400"></i>
-      <span class="truncate">{node.name}</span>
-    </div>
-
-    <div class="opacity-0 group-hover:opacity-100 flex gap-2 text-zinc-500">
-      {#if node.type === "folder"}
-        <i class="ri-file-add-line cursor-pointer" on:click={() => newFile(node.id)}></i>
-        <i class="ri-folder-add-line cursor-pointer" on:click={() => newFolder(node.id)}></i>
-      {/if}
-      <i class="ri-edit-line cursor-pointer" on:click={() => rename(node)}></i>
-      <i class="ri-delete-bin-line cursor-pointer" on:click={() => del(node)}></i>
-    </div>
-  </div>
-
-  {#if node.type === "folder" && node.isOpen}
-    {#each node.children as child}
-      <TreeItem node={child} depth={depth + 1}/>
-    {/each}
-  {/if}
 </div>
